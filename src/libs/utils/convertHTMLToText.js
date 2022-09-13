d44ca425919447c34e9701a8a5b963a4516eb9f0
Liz Kaufman <lizkaufman92@gmail.com>
2022-09-13 16:56:12 +0100

    purposefully breaking test again to check CI will fail

--- src/libs/utils/convertHTMLToText.js
+++ src/libs/utils/convertHTMLToText.js
@@ -1,7 +1,7 @@
-export default function convertHTMLToText(html) {
-  let text = html;
-  text = text.replace(/<[^>]+>/g, " ");
-  text = text.replace(/\n/g, " ");
-  text = text.replace(/\r/g, " ");
-  return text;
-}
+// export default function convertHTMLToText(html) {
+//   let text = html;
+//   text = text.replace(/<[^>]+>/g, " ");
+//   text = text.replace(/\n/g, " ");
+//   text = text.replace(/\r/g, " ");
+//   return text;
+// }

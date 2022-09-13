f5aebcba4833544b5ab2c36517ee2cfe841e6a10
Liz Kaufman <lizkaufman92@gmail.com>
2022-09-13 16:50:05 +0100

    purposefully breaking test again to check CI will fail

--- src/libs/utils/countWords.js
+++ src/libs/utils/countWords.js
@@ -3,7 +3,7 @@ import convertHTMLToText from "./convertHTMLToText";
 export default function countWords(string) {
   let text = string;
   text = convertHTMLToText(string);
-  let splitText = text.split(" ");
-  splitText = splitText.filter((word) => (word === "" ? false : true));
+  // let splitText = text.split(" ");
+  // splitText = splitText.filter((word) => (word === "" ? false : true));
   // return splitText.length;
 }
